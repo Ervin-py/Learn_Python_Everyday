5dae272b6f88857c0e0fb6025e9d36aacc5fa756
Ervin <ervinadoptante@gmail.com>
2026-08-18 20:02:19 +0800

    Day 7

--- 7.py
+++ 7.py
@@ -23,7 +23,8 @@ drawing = [
 while True:
     print("1. Search drawing")
     print("2. Add drawing")
-    print("3. Add drawing")
+    print("3. Update drawing")
+    print("4. Update drawing")
     choose = input("Enter number you want to do: ")
 
     if choose == "1":
@@ -37,15 +38,14 @@ while True:
                 found = False
 
                 for state in drawing:
-                    if state["drawing_number"] == search:  
+                    if state["drawing_number"] == search:
                         found = True
                         for key, value in state.items():
                             print(f"{key} : {value}")
 
 
-                if found == False:
-                    print("Drawing not found") 
-
+                if not found:
+                    print("Drawing not found")
             else:
                 print("Invalid input")
 
@@ -55,16 +55,16 @@ while True:
             if user_input == "y":
                 break
 
-            elif user_input == "n":          
+            elif user_input == "n":
                 search = input("Enter drawing_number: ")
                 found = False
 
                 for state in drawing:
-                    if state["drawing_number"] == search:  
+                    if state["drawing_number"] == search:
                         found = True
                         print("Drawing already exists")
 
-                if found == False:  
+                if not found:
                     name = input("Enter name: ")
                     revision = input("Enter revision: ")
                     status = input("Enter status: ")
@@ -74,7 +74,7 @@ while True:
                         "revision": revision,
                         "status": status
                     })
-
+                    print("Added succesful") 
             else:
                 print("Invalid input")                  
 
@@ -85,42 +85,80 @@ while True:
                 break
 
             elif user_input == "n":
-                search = input("Enter drawing_number you want to update: ")
+                search_2 = input("Enter drawing_number you want to update: ")
                 found = False
 
-                for state in drawing:
-                    if state["drawing_number"] == search:  
+                for state_2 in drawing:
+                    if state_2["drawing_number"] == search_2:
                         found = True
-                        for key, value in state.items():
+                        for key, value in state_2.items():
+                            print("Current state: ")
                             print(f"{key} : {value}")
-                            print("Enter number you want to update")
                             print("1. Drawing number")                            
                             print("2. Drawing name")
                             print("3. Status")                            
                             print("4. Revision")
+                            print("5. Exit")
                             update = input("Enter here: ")                            
                             if update == "1":
                                 update_number = input("Enter new number: ")
-                                state["drawing_number"] = update_number
-                                print(drawing)
+                                exists = False
+                                        
+                                for item_2 in drawing:
+                                    if item_2["drawing_number"] == update_number:
+                                        exists = True
+                                        print("Drawing is already named the same")
+                                            
+                                    else:
+                                        item_2["drawing_number"] = update_number
+                                        print(drawing)
+                                            
                             elif update == "2":
                                 update_name = input("Enter new name: ")
-                                state["name"] = update_name
+                                state_2["name"] = update_name
                                 print(drawing)
+                            
                             elif update == "3":
                                 update_status = input("Enter new status: ")
-                                state["status"] = update_status
+                                state_2["status"] = update_status
                                 print(drawing)
+                            
                             elif update == "4":
                                 update_revision = input("Enter new revision: ")
-                                state["revision"] = update_revision
+                                state_2["revision"] = update_revision
                                 print(drawing)
-
-                if found == False:
+                            
+                            elif update == "5":
+                                break
+                                    
+                if not found:
                     print("Drawing not found") 
 
             else:
                 print("Invalid input")
-  
+
+    if choose == "4":
+        while True:
+            user_input = input("Exit? y/n: ")
+            if user_input == "y":
+                break
+
+            elif user_input == "n":
+                delete = input("Enter drawing_number you want to delete: ")
+                found = False
+
+                for state_4 in drawing:
+                    if state_4["drawing_number"] == delete:
+                        found = True
+                        for key_4, value_4 in state_4.items():
+                        del key_4,
+                        del value_4
+                        print(f"Deleted succesfully")
+
+
+                if not found:
+                    print("Drawing not found")
+            else:
+                print("Invalid input")  
 
 
